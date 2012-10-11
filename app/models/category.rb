34334496f26243f49723eee0b4ad1a2fe610b636
Siri Hedengran <siricat@gmail.com>
2012-10-10 19:14:29 -0700

    create category relationships

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ActiveRecord::Base
+  attr_accessible :name
+  
+  belongs_to  :user
+  has_many    :products
+  
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to  :user
+  belongs_to  :category
+  
 end

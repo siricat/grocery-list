e8d9c251bc7351f14a54fb74fd449636dadfb1b6
Siri Hedengran <siricat@gmail.com>
2012-10-12 11:29:14 -0700

    associate product to categories

--- db/migrate/20121012182252_create_products.rb
+++ db/migrate/20121012182252_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string      :name
-      t.string      :category
+      t.references  :category
       t.references  :user
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121005192759) do
+ActiveRecord::Schema.define(:version => 20121012182252) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "list_items", :force => true do |t|
     t.string   "quantity"
@@ -31,10 +38,10 @@ ActiveRecord::Schema.define(:version => 20121005192759) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
-    t.string   "category"
+    t.integer  "category_id"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|

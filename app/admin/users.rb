2b782ba76eb007d8ff6045a20b34a93198042781
Neil Heinrich <nheinrich@gmail.com>
2012-10-18 21:25:46 -0700

    update active_admin initializer to be a bit cleaner

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,22 +1,22 @@
 ActiveAdmin.register User do
-  
-  index do   
-    selectable_column                           
-    column :id                  
-    column :display_name        
+
+  index do
+    selectable_column
+    column :id
+    column :display_name
     column :email
     column "Created on", :created_at
-    column "Last Signed in", :last_sign_in_at    
+    column "Last Signed in", :last_sign_in_at
     column :sign_in_count, :class => "sign_in_count"
-    default_actions                   
+    default_actions
   end
-  
+
   filter :display_name
   filter :email
-  
+
   config.sort_order = "display_name_asc"
-  config.per_page = 50  
-  
+  config.per_page = 50
+
   form do |f|
     f.inputs "New User" do
       f.input :display_name
@@ -25,13 +25,11 @@ ActiveAdmin.register User do
       f.input :password_confirmation
     end
     f.buttons
-  end  
-  
+  end
+
   batch_action :destroy, :confirm => "Are you sure you want to delete all of these?" do |selection|
     User.find(selection).each { |u| u.destroy }
     redirect_to collection_path, :notice => "Selected users deleted."
-  end  
-  
-  config.comments = false  
-  
+  end
+
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -10,7 +10,7 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
@@ -100,7 +100,7 @@ ActiveAdmin.setup do |config|
   # Admin comments are enabled by default.
   #
   # Default:
-  # config.allow_comments = true
+  config.allow_comments = false
   #
   # You can turn them on and off for any given namespace by using a
   # namespace config block.
@@ -116,7 +116,7 @@ ActiveAdmin.setup do |config|
   # Enable and disable Batch Actions
   #
   config.batch_actions = true
-  
+
 
   # == Controller Filters
   #
@@ -133,13 +133,15 @@ ActiveAdmin.setup do |config|
   # and feel.
   #
   # To load a stylesheet:
-  #   config.register_stylesheet 'my_stylesheet.css'
-  
+  config.clear_stylesheets!
+  config.register_stylesheet 'admin.css'
+
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
   #   config.register_stylesheet 'my_print_stylesheet.css', :media => :print
   #
   # To load a javascript file:
-  #   config.register_javascript 'my_javascript.js'
+  config.clear_javascripts!
+  config.register_javascript 'admin.js'
 
 
   # == CSV options
